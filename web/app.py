41b1568d79f91641bc977c7a2b7ce167105ec2ca
stdevelopr <stdevelopr@gmail.com>
2020-01-17 14:44:41 -0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -78,4 +78,4 @@ api.add_resource(Visit, "/visit")
 #Run the application
 ############################################
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0")
